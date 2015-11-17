dabea887ecbfa8bbbf86bc0f66a912443d2d6d7b
Jean-Marc Vitali <jean-marc.vitali@amadeus.com>
2015-11-17 11:05:44 +0100

    Cerberus: Updated social code to load Cerberus configuration.

--- pkg/setting/setting_oauth.go
+++ pkg/setting/setting_oauth.go
@@ -4,14 +4,16 @@ type OAuthInfo struct {
 	ClientId, ClientSecret string
 	Scopes                 []string
 	AuthUrl, TokenUrl      string
+    ReqTokenUrl            string
 	Enabled                bool
 	AllowedDomains         []string
 	ApiUrl                 string
 	AllowSignup            bool
+    AutoSignUp             bool
 }
 
 type OAuther struct {
-	GitHub, Google, Twitter bool
+	GitHub, Google, Twitter, Cerberus bool
 	OAuthInfos              map[string]*OAuthInfo
 }
 
